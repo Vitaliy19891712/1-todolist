3fa6ac577004d2c7b27b78e3aa03cd23de4df086
Vitaliy19891712 <stopkaiko@mail.ru>
2023-01-06 20:41:55 +0300

    1

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import TodoList, { TaskType } from "./TodoList";
+
+function App() {
+  const todoListTitle_1: string = "What to learn";
+  const todoListTitle_2: string = "What to buy";
+  const tasks_1: Array<TaskType> = [
+    { id: 1, title: "HTML", isDone: true },
+    { id: 2, title: "CSS", isDone: true },
+    { id: 3, title: "JS/TS", isDone: false },
+  ];
+  const tasks_2: Array<TaskType> = [
+    { id: 1, title: "Apple", isDone: true },
+    { id: 2, title: "Meat", isDone: true },
+    { id: 3, title: "Veg", isDone: false },
+  ];
+  return (
+    <div className="App">
+      <TodoList title={todoListTitle_1} tasks={tasks_1} />
+      <TodoList title={todoListTitle_2} tasks={tasks_2} />
+    </div>
+  );
+}
+export default App;
---
+++ src/TodoList.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+type TodoListPropsType = {
+  title: string;
+  tasks: Array<TaskType>;
+};
+
+export type TaskType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+const TodoList = (props: TodoListPropsType) => {
+  let tasksList = props.tasks.length ? (
+    props.tasks.map((task: TaskType) => {
+      return (
+        <li>
+          <input type="checkbox" checked={task.isDone} />
+          <span>{task.title}</span>
+        </li>
+      );
+    })
+  ) : (
+    <span>Your taskslist is empty</span>
+  );
+
+  return (
+    <>
+      <h3>{props.title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        <div>{tasksList}</div>
+      </ul>
+
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
