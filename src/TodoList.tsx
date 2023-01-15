a8bdfe61812cf357c3b493d1921c956db2ba6d2d
Vitaliy19891712 <stopkaiko@mail.ru>
2023-01-15 15:46:34 +0300

    commit after second lesson

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,23 +1,37 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
 
+// Типизируем ТудуЛист
 type TodoListPropsType = {
   title: string;
   tasks: Array<TaskType>;
+  removeTask: (taskId: number) => void;
+  changeFilter: (filter: "all" | "active" | "completed") => void;
 };
 
+// Типизируем Таски (наши задачи)
 export type TaskType = {
   id: number;
   title: string;
   isDone: boolean;
 };
 
+// Создание компоненты ТудуЛист
 const TodoList = (props: TodoListPropsType) => {
+  // Условный рендеринг (condition rendering)
+  // В зависимости от того или иного условия
+  // мы рендерим ту или иную разметку в компоненте
   let tasksList = props.tasks.length ? (
     props.tasks.map((task: TaskType) => {
+      // Переменной removeTask присваиваем функцию
+      // с параметром (id), что бы в компоненте Арр описать
+      // что делает эта функция(удаляет таску)
+      const removeTask = () => props.removeTask(task.id);
       return (
         <li>
           <input type="checkbox" checked={task.isDone} />
-          <span>{task.title}</span>
+          <span>{task.title} </span>
+          <button onClick={removeTask}>x</button>
         </li>
       );
     })
@@ -25,8 +39,9 @@ const TodoList = (props: TodoListPropsType) => {
     <span>Your taskslist is empty</span>
   );
 
+  //То, что рендерится
   return (
-    <>
+    <div>
       <h3>{props.title}</h3>
       <div>
         <input />
@@ -35,13 +50,16 @@ const TodoList = (props: TodoListPropsType) => {
       <ul>
         <div>{tasksList}</div>
       </ul>
-
       <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        {/* Кнопкам прописали, что при клике вызывается функция 
+       с определнным параметром а в компоненте Арр пропишем, что она будет делать */}
+        <button onClick={() => props.changeFilter("all")}>All</button>
+        <button onClick={() => props.changeFilter("active")}>Active</button>
+        <button onClick={() => props.changeFilter("completed")}>
+          Completed
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
